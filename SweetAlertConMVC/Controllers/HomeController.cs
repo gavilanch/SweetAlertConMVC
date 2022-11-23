e89b8d4b5f5af11bc4e85ce82629ae0e532cc022
Felipe Gavilan <elmejor_887@hotmail.com>
2022-11-22 20:16:05 -0400

    Add project files.

---
+++ SweetAlertConMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SweetAlertConMVC.Models;
+using System.Diagnostics;
+
+namespace SweetAlertConMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Nombre)
+        {
+            return RedirectToAction("Completado", new { Nombre });
+        }
+
+        public IActionResult Completado(string Nombre)
+        {
+            return View("Completado", Nombre);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SweetAlertConMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SweetAlertConMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
